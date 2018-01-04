97eedf7dca0776b238d07c5ec49b6e15881145f2
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 19:57:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const returnFirstTwoDrivers = function (drivers) {
   copy.push(drivers[0], drivers[1])
   return copy
 }
+
+const returnLastTwoDrivers = function (drivers) {
+  const copy = []
+  copy.push(drivers[drivers.length - 2], drivers[drivers.length - 1])
+  return copy
+}
